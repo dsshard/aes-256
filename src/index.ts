396c8825f214bf988ab741260e94613311f8c572
Steven Green <dsshard@gmail.com>
2022-05-08 15:35:12 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import crypto from 'crypto'
+
+export class Aes256 {
+  private readonly salt: string;
+  private readonly iv: Buffer;
+
+  constructor (salt: string, iv: Buffer) {
+    this.salt = salt
+    this.iv = iv
+  }
+
+  encrypt (text: string, password: string): string {
+    const key = crypto.scryptSync(password, this.salt, 32)
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, this.iv)
+    let encrypted = cipher.update(text)
+    encrypted = Buffer.concat([encrypted, cipher.final()])
+    return encrypted.toString('hex')
+  }
+
+  decrypt (text: string, password: string): string {
+    const key = crypto.scryptSync(password, this.salt, 32)
+    const encryptedText = Buffer.from(text, 'hex')
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, this.iv)
+    let decrypted = decipher.update(encryptedText)
+    decrypted = Buffer.concat([decrypted, decipher.final()])
+    return decrypted.toString()
+  }
+}
